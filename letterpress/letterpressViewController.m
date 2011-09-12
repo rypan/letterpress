1389f959f02d5371e7e95a08eb5c2d71eeb156b9
Ryan Panchadsaram <ryanpanchadsaram@gmail.com>
2011-09-11 22:51:13 -0700

    Initial push. Still a work in progress.

--- letterpress/letterpressViewController.m
+++ letterpress/letterpressViewController.m
@@ -22,6 +22,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int pageWidth;
+    int pageHeight;
+    int pageShift;
+    int numberOfPages;
+    int counter;
+    
+    numberOfPages = 3;
+    
+    pageWidth = 1024;
+    pageHeight = 748;
+    pageShift = 1025;
+    
+    
+    //self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    
+    self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, pageWidth, pageHeight)];
+    
+    
+    CGRect scrollViewFrame = self.view.frame;
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:scrollViewFrame];
+    [self.view addSubview:scrollView];
+    
+    CGSize scrollViewContentSize = CGSizeMake(pageWidth * numberOfPages, pageHeight);
+    [scrollView setContentSize:scrollViewContentSize];
+    
+    for (counter = 0; counter<3; counter++) {
+        UIImageView *imageN = [[UIImageView alloc] initWithFrame:CGRectMake(pageWidth*counter, 0, pageWidth, pageHeight)];
+        
+        NSLog(@"%@",[NSString stringWithFormat:@"page%d.png",counter+1]);
+        
+        imageN.image = [UIImage imageNamed:[NSString stringWithFormat:@"page%d.png",counter+1]];
+        [scrollView addSubview:imageN];
+    }
+    
+    [scrollView setPagingEnabled:YES];
+    [scrollView setShowsHorizontalScrollIndicator:NO];
+    [scrollView setShowsVerticalScrollIndicator:NO];
+    
+    
 }
 
 - (void)viewDidUnload
